7f4a4198ac4cbbf0316fde5757bb7bfedc690a6e
Marius <marius@example.com>
2022-05-30 10:33:10 +0300

    terenary divide

--- es6mod.js
+++ es6mod.js
@@ -1,4 +1,5 @@
 import sum, { add } from './modules/add.js';
+import { divide } from './modules/divide.js';
 
 console.log(add(1, 2, 3));
 
@@ -9,3 +10,5 @@ console.log('sum ===', sum);
 // grazina tu argumentu dalyba
 // jei antras argumentas 0 grazina texta 'dalinti is 0 negalima'
 // panaudojam funkcija es6mod.js faile
+console.log('divide(15, 3) ===', divide(15, 3));
+console.log('divide(15, 0) ===', divide(15, 0));
